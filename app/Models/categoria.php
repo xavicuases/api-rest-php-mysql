60bcb92818c0fb77d1fdc4704b6353412b32d596
Xavier Cuases <xaviercuases@hotmail.com>
2026-07-24 17:33:51 -0500

    feat: :sparkles: creacion del modelo Categoria

--- app/Models/categoria.php
+++ app/Models/categoria.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class categoria extends Model
 {
-    //
+    public $timetams = false;
+    protected $fillable = ['id','cat_nom','cat_obs'];
+
 }
