73ab68d789aec6067bf18da0fc890eccc9b95c83
Xavier Cuases <xaviercuases@hotmail.com>
2026-07-24 20:31:26 -0500

    feat: :sparkles: implement POST and PUT endpoints for category creation and updates

--- app/Http/Controllers/categoriacontroller.php
+++ app/Http/Controllers/categoriacontroller.php
@@ -20,4 +20,22 @@ class categoriacontroller extends Controller
         }
         return response()->json($categoria::find($id),200);
     }
+
+    public function insertCategoria(Request $request){
+        $categoria = categoria::create($request->all());
+        return response($categoria,200);
+    }
+
+    public function updateCategoria(Request $request, $id)
+{
+    $categoria = Categoria::find($id);
+
+    if (is_null($categoria)) {
+        return response()->json(['Mensaje' => 'Registro no encontrado'], 404);
+    }
+
+    $categoria->update($request->all());
+
+    return response()->json($categoria, 200);
+}
 }
--- routes/api.php
+++ routes/api.php
@@ -11,3 +11,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request){
 Route::get('/categoria', [\App\Http\Controllers\categoriacontroller::class, 'getCategoria']);
 
 Route::get('/categoria/{id}', [\App\Http\Controllers\categoriacontroller::class, 'getCategoriaxid']);
+
+Route::post('addCategoria', [\App\Http\Controllers\categoriacontroller::class, 'insertCategoria']);
+
+Route::put('updateCategoria/{id}', [\App\Http\Controllers\categoriacontroller::class, 'updateCategoria']);
